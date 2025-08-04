a79c79208f91228dde432fa8a652fefb54eba6e6
luminecent-nard <stapletonr4257@masseyhigh.school.nz>
2025-08-05 10:45:35 +1200

    variable basics

---
+++ P_02_variables.py
@@ -0,0 +1,21 @@
+#code to learn about variables
+
+#variables can be a string (note the speech marks)
+greeting = "hello world"
+
+#variables can be a number
+fav_num = 7
+
+#variables can be boolean (true/false)
+love_programming = True
+
+num_string = 5
+
+print(fav_num * 2)
+print(num_string * 5)
+#this code has a problem
+my_name = "Rocco"
+print(greeting)
+print(fav_num)
+print(love_programming)
+print(my_name)
