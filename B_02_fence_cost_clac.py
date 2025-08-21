93ffcfe2daa675f6a0642496c85ece528591465f
luminecent-nard <stapletonr4257@masseyhigh.school.nz>
2025-08-22 10:47:04 +1200

    fence calculator stuff

---
+++ B_02_fence_cost_clac.py
@@ -0,0 +1,38 @@
+def num_check(question):
+    """ask user for width and loop until they enter a number that is more than 0"""
+    error1 = "please pick a number above zero\n you want to calculate stuff, dont you?"
+    error2 = "please pick a NUMBER to continue calculating"
+    while True:
+
+        try:
+            #ask user for width
+            response = float(input(question))
+
+            #check that width is more than 0
+            if response > 0:
+                return response
+            else:
+                print(error1)
+        except ValueError:
+            print(error2)
+#main routine starts here
+
+keep_going = ""
+while keep_going == "":
+
+    #get width and height
+    width = num_check("width: ")
+    height = num_check("height: ")
+    cost = num_check("cost per unit:")
+    #calculate area perimeter
+
+    perimeter = 2 * (height + width)
+    price = perimeter * cost
+    #display output
+    print()
+    print(f"the perimeter is {perimeter}")
+    print(f"the price is ${price:.2f} units")
+    #ask user if they want to keep going
+    keep_going = input("press any key to exit or enter to go again")
+
+print("thank you for using the area perimeter calculator")
